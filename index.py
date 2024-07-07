097b18b7a4d8867ffb334b3377d98d6f4cca2455
Enigma987 <44814640+Enigma987@users.noreply.github.com>
2024-07-07 17:04:40 +0200

    arena enter

--- index.py
+++ index.py
@@ -1,1 +1,7 @@
 print("Welcome to the Arena of the Archi. World's gratest arena in this land")
+print("You want to enter?")
+
+if input() == "Y" or "y":
+    print("You've enter to the arena")
+else:
+    print("You've come back to City")
